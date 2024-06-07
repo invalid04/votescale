a95d4351667df5da1d52af7da19830a976fbc778
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-07 03:10:08 -0500

    added components from airbnb ui library

--- client/src/app/[topic]/ClientPage.tsx
+++ client/src/app/[topic]/ClientPage.tsx
@@ -2,14 +2,27 @@
 
 import MaxWidthWrapper from "@/components/MaxWidthWrapper"
 import { useState } from "react"
+import { Wordcloud } from "@visx/wordcloud"
+import { scaleLog } from "@visx/scale"
+import { Text } from "@visx/text"
 
 interface ClientPageProps {
     topicName: string 
     initialData: {text: string, value: number }[]
 }
 
+const COLORS = ['#143059', '#2F6B9A', '#82A6C2']
+
 const ClientPage = ({topicName, initialData}: ClientPageProps) => {
     const [words, setWords] = useState(initialData)
+
+    const fontScale = scaleLog({
+        domain: [
+            Math.min(...words.map((w) => w.value)), 
+            Math.max(...words.map((w) => w.value)),
+        ],
+        range: [10, 100]
+    }) 
 
     return (
         <div className='w-full flex flex-col items-center justify-center min-h-screen bg-grid-zinc-50 pb-20'>
@@ -18,8 +31,37 @@ const ClientPage = ({topicName, initialData}: ClientPageProps) => {
                     What people think about{" "}
                     <span className='text-blue-600'>
                         {topicName}
-                    </span>
+                    </span>:
                 </h1>
+
+                <p className='text-sm'>(updated in real-time)</p>
+
+                <div className='aspect-square max-w-xl flex items-center justify-center'>
+                    <Wordcloud
+                        words={words}
+                        width={500}
+                        height={500}
+                        fontSize={(data) => fontScale(data.value)}
+                        font={'Impact'}
+                        padding={2}
+                        spiral='archimedean'
+                        rotate={0}
+                        random={() => 0.5}
+                    >
+                        {(cloudWords) => cloudWords.map((w, i) => (
+                            <Text 
+                                key={w.text} 
+                                fill={COLORS[i % COLORS.length]}
+                                textAnchor='middle'
+                                transform={`translate(${w.x}, ${w.y})`}
+                                fontSize={w.size}
+                                fontFamily={w.font}
+                            >
+                                {w.text}
+                            </Text>
+                        ))}
+                    </Wordcloud>
+                </div>
             </MaxWidthWrapper>
         </div>
     )
