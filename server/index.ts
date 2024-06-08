a03933e157a0d55ee3d420351111e7dbcbb9808f
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-08 01:04:09 -0500

    fixed socket connection issue

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,14 @@ import express from "express"
 import cors from "cors"
 import http from "http"
 import { Server } from "socket.io"
+import { Redis } from "ioredis"
+import "dotenv/config"
 
 const app = express()
 app.use(cors())
+
+const redis = new Redis(process.env.REDIS_CONNECTION_STRING)
+const subRedis = new Redis(process.env.REDIS_CONNECTION_STRING)
 
 const server = http.createServer(app)
 const io = new Server(server, {
@@ -20,6 +25,22 @@ io.on("connection", async (socket) => {
 
     socket.on("join-room", async (room: string) => {
         console.log('user joined room:', room)
+
+        const subscribedRooms = await redis.smembers("subscribed-rooms")
+        await socket.join(room)
+        await redis.sadd(`rooms:${id}`, room)
+        await redis.hincrby("room-connections", room, 1)
+
+        if(!subscribedRooms.includes(room)) {
+            subRedis.subscribe(room, async (err) => {
+                if(err) {
+                    console.error('Failed to subscribe', err)
+                } else {
+                    await redis.sadd('subscribed-rooms', room)
+                    console.log("Subscribed to room:", room)
+                }
+            })
+        }
     })
 })
 
